4db95006178a3200dce3fb59021475cc53bb320a
fran-cisko <franciscosciarretta@gmail.com>
2022-02-10 16:41:40 -0300

    I add json file to body texts and css styles

---
+++ main.js
@@ -0,0 +1,10 @@
+//web's text.
+const init = async () => {
+	const config = await (await fetch('./config.json')).json();
+	let newHTML = document.documentElement.innerHTML;
+	Object.keys(config).map((key) => {
+		newHTML = newHTML.replace(key, config[key]);
+	});
+	document.documentElement.innerHTML = newHTML;
+};
+init();
